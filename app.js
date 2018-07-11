133aa1076200b1a085fd395990f209d0e1dae99b
mhd.ihsan <mhd.ihsan99@gmail.com>
2018-07-12 00:33:29 +0700

    struktur data penyimpanan

--- app.js
+++ app.js
@@ -1,7 +1,61 @@
 //Budget controller
 
 const budgetController = (function(){
-    //
+    let Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value =value;
+    };
+
+    let Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value =value;
+    };
+
+    let data = {
+        allItems : {
+            exp : [],
+            inc : [],
+        },
+
+        totals : {
+            exp : 0,
+            inc : 0,
+        }
+    }
+    return {
+        addItem : function(type, des, val){
+            let newItem,ID;
+            
+            //membuat id baru
+            if (data.allItems[type]>0)
+            {
+                ID = data.allItems[type] [data.allItems[type].length - 1].id + 1;}
+            else {
+                ID = 0;
+            }
+            
+
+            //membuat item baru
+            if (type==='exp'){
+                newItem = new Expense (ID,des,val)
+            }
+            else if (type === 'inc')
+            {
+                newItem = new Expense (ID, des,val)
+            }
+            //push data baru
+            data.allItems[type].push(newItem)
+
+            //kembalikan elemen baru
+            return newItem
+        },
+        testing : function () {
+            console.log(data)
+        }
+    }
+    
 })();
 
 
@@ -39,13 +93,15 @@ const controller =  (function(budgetController,UIController){
 
     let setEvenlistener = function  (){
         let DOM = UIController.getDOMstring()//panggil DOMstring global dari UIcontroller
+
         document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem)
 
-    document.addEventListener('keypress', function (event) {
+
+        document.addEventListener('keypress', function (event) {
         if (event.keyCode===13 || event.which===13){
-            ctrlAddItm()
-        }
-    })
+            ctrlAddItem()
+            }
+        })
     }
     
     
@@ -56,7 +112,7 @@ const controller =  (function(budgetController,UIController){
         let input = UIController.getinput();
         console.log (input)
         //2.masukan item ke budget cnotroller
-
+        let newItem = budgetController.addItem(input.type, input.description, input.value)
         //3.add item ke user intervace
 
         //4. calculate budget
@@ -68,7 +124,7 @@ const controller =  (function(budgetController,UIController){
     return {
         init : function (){
             console.log (`server jalan`);
-            setEvenlistener();
+            return setEvenlistener();
         }
     }
 
