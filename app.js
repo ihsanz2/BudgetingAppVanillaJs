8fe8f449015422363af5ec0ab6496c7b3d975510
mhd.ihsan <mhd.ihsan99@gmail.com>
2018-07-11 12:35:30 +0700

    module input data

--- app.js
+++ app.js
@@ -8,7 +8,28 @@ const budgetController = (function(){
 
 //UI controller
 const UIController = (function (){
+        //Object DOMstring local(closeure)
+        let DOMstring ={
+            inputType : '.add__type',
+            inputDescription : '.add__description',
+            inputValue : '.add__value',
+            inputBtn : '.add__btn'
+            
+        }
 
+        return {
+            getinput : function () {
+                return {
+                    type : document.querySelector(DOMstring.inputType).value,
+                    description : document.querySelector(DOMstring.inputDescription).value,
+                    value : document.querySelector(DOMstring.inputValue).value
+                }
+            },
+            //membuat object DOMstring menjadi Global
+            getDOMstring:function(){
+                return DOMstring;
+            }
+        }
 })();
 
 
@@ -16,8 +37,45 @@ const UIController = (function (){
 //global App controller
 const controller =  (function(budgetController,UIController){
 
-    document.querySelector('.add__btn').addEventListener('click',function(){
-        console.log(`botton di click`)
+    let setEvenlistener = function  (){
+        let DOM = UIController.getDOMstring()//panggil DOMstring global dari UIcontroller
+        document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem)
+
+    document.addEventListener('keypress', function (event) {
+        if (event.keyCode===13 || event.which===13){
+            ctrlAddItm()
+        }
     })
+    }
+    
+    
+
+
+    let ctrlAddItem = function (){
+        //1. ambil input data
+        let input = UIController.getinput();
+        console.log (input)
+        //2.masukan item ke budget cnotroller
+
+        //3.add item ke user intervace
+
+        //4. calculate budget
+
+        //5. display budget
+        
+    }
+
+    return {
+        init : function (){
+            console.log (`server jalan`);
+            setEvenlistener();
+        }
+    }
+
+    
 
 })(budgetController, UIController)
+
+controller.init()
+
+
